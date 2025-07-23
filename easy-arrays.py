5d17587999f5fbaaad40fe159c184b4dba2ced31
Dayalan <85922079+daya1an@users.noreply.github.com>
2025-07-24 02:09:26 +0530

    Update and rename arrays.py to easy-arrays.py

---
+++ easy-arrays.py
@@ -0,0 +1,25 @@
+# 1. https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=array
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if (nums[i] + nums[j] == target):
+                    ans.append(i)
+                    ans.append(j)
+                    return ans
+
+# 2. https://leetcode.com/problems/search-insert-position/?envType=problem-list-v2&envId=array
+def searchInsert(self, nums: List[int], target: int) -> int:
+        res = len(nums)
+        if target in nums:
+            res = nums.index(target)
+        elif nums[0] >= target:
+            res = 0
+        else:
+            for i in range(1,len(nums)):
+                if nums[i-1] < target and nums[i] > target:
+                    res = i
+                    break
+        return res
