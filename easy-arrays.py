5ad6abc4a2389fcf7ece003623b7e47c574cfbd2
Dayalan <85922079+daya1an@users.noreply.github.com>
2025-08-11 22:07:16 +0530

    added intersection-of-two-arrays-ii

--- easy-arrays.py
+++ easy-arrays.py
@@ -41,3 +41,27 @@ def romanToInt(self, s: str) -> int:
                 ans = ans + roman[s[i]]
         
         return ans + roman[s[-1]]
+
+# 4. http://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        op = []
+        i = 0
+        j = 0
+        
+        nums1.sort()
+        nums2.sort()
+        
+        while (i < len(nums1) and j < len(nums2)):
+            if nums1[i] == nums2[j]:
+                op.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i] > nums2[j]:
+                j+=1
+            else:
+                i+=1
+        
+        return op
