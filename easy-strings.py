1e0a42f9e9601ee82627553d4707cafddd308d34
Dayalan D S <dayalands2210@gmail.com>
2025-08-21 15:08:02 +0530

    removing class

--- easy-strings.py
+++ easy-strings.py
@@ -1,78 +1,74 @@
 #   Reverse Integer
 
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x > 0:
-            x = int(str(x)[::-1])
-        else:
-            x = int(str(abs(x))[::-1]) * -1
-        
-        if x < -2**31 or x > 2**31 -1:
-            x = 0
-        
-        return x
+def reverse(self, x: int) -> int:
+    if x > 0:
+        x = int(str(x)[::-1])
+    else:
+        x = int(str(abs(x))[::-1]) * -1
+    
+    if x < -2**31 or x > 2**31 -1:
+        x = 0
+    
+    return x
 
 #   First Unique Character in a String
 
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        cnt = {}
-        op = -1
-        for i in range(len(s)):
-            if s[i] in cnt:
-                cnt[s[i]] +=1
-            else:
-                cnt[s[i]] = 1
-        
-        for e in cnt:
-            if int(cnt[e]) == 1:
-                return s.index(e)
-        
-        return -1
+def firstUniqChar(self, s: str) -> int:
+    cnt = {}
+    op = -1
+    for i in range(len(s)):
+        if s[i] in cnt:
+            cnt[s[i]] +=1
+        else:
+            cnt[s[i]] = 1
+    
+    for e in cnt:
+        if int(cnt[e]) == 1:
+            return s.index(e)
+    
+    return -1
 
 #   Valid Anagram
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t) and sorted(s) == sorted(t):
-            return True
-        return False
+def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) == len(t) and sorted(s) == sorted(t):
+        return True
+    return False
 
 #     String to Integer (atoi)
-class Solution:
-    def myAtoi(self, s: str) -> int:
-        op = ""
-        s = s.lstrip()
-        
-        #checking empty string and starting characters
-        if s == "" or s[0].isalpha() or s[0] == ".":
-            print("RETURN 1")
-            return 0
-        
-        sign = 1
-        i = 0
-        
-        # Handle sign
-        if s[0] in ['-', '+']:
-            if s[0] == '-':
-                sign = -1
-            i += 1
-        
-        # Parse digits
-        num = 0
-        while i < len(s) and s[i].isdigit():
-            num = num * 10 + int(s[i])
-            i += 1
-        
-        num *= sign
+def myAtoi(self, s: str) -> int:
+    op = ""
+    s = s.lstrip()
+    
+    #checking empty string and starting characters
+    if s == "" or s[0].isalpha() or s[0] == ".":
+        print("RETURN 1")
+        return 0
+    
+    sign = 1
+    i = 0
+    
+    # Handle sign
+    if s[0] in ['-', '+']:
+        if s[0] == '-':
+            sign = -1
+        i += 1
+    
+    # Parse digits
+    num = 0
+    while i < len(s) and s[i].isdigit():
+        num = num * 10 + int(s[i])
+        i += 1
+    
+    num *= sign
 
-        # handle max and min values     
-        if num > 2**31 -1:
-            print("RETURN 6")
-            return 2**31-1
-        
-        if num < -2**31:
-            print("RETURN 7")
-            return -2**31
-        
-        return num
+    # handle max and min values     
+    if num > 2**31 -1:
+        print("RETURN 6")
+        return 2**31-1
+    
+    if num < -2**31:
+        print("RETURN 7")
+        return -2**31
+    
+    return num
