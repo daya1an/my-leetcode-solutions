c1b58a973fc62f9489831232a9833d70c839a196
Dayalan D S <dayalands2210@gmail.com>
2025-10-16 02:42:20 +0530

    Oct 16 updates

--- Easy/Trees.py
+++ Easy/Trees.py
@@ -11,3 +11,42 @@ class Solution:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# https://leetcode.com/problems/validate-binary-search-tree/
+
+#my solution (3ms)
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stk = []
+        prev = float('-inf')
+        curr = root
+
+        while curr or stk:
+            while curr:
+                stk.append(curr)
+                curr = curr.left
+
+            curr = stk.pop()
+
+            if prev >= curr.val:
+                return False
+
+            prev = curr.val
+            curr = curr.right
+
+        return True
+
+
+# 0ms solution
+class Solution:
+    def validate(self, node, low, high):
+        if not node:
+            return True
+
+        if not (low < node.val < high):
+            return False
+
+        return self.validate(node.left, low, node.val) and self.validate(node.right, node.val, high)
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.validate(root, float('-inf'), float('inf'))
